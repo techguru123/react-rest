2e6d1824135eecac8a6c6b194a26bb8ec9b571a5
sdnyanesh352 <sdnyanesh352@gmail.com>
2022-09-05 23:28:05 +0530

    fst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [apiData, setApiData]=useState([])
+  useEffect(()=>{
+    async function callApi(){
+      const apiResult= await axios.get('https://raw.githubusercontent.com/techguru123/MoodMob/master/data.json');
+      setApiData(apiResult.data);
+      
+    }
+    callApi();
+    console.log(apiData);
+  }, [apiData])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <table>
+        <thead>
+        <tr>
+          <th>Id </th>
+          <th>Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          apiData &&
+          (apiData).map((apiDetails,index)=>{
+
+            return(
+              <tr key={index}>
+                <td>{apiDetails.id}</td>
+              </tr>
+            )
+          })
+        }
+        </tbody>
+       </table>
       </header>
     </div>
+    
   );
+
+
 }
 
 export default App;
